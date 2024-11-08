abfb15533fac0f0d1c1ee70bb732e0baca3a4310
prasad9600 <prasadkumar1998@gmail.com>
2024-11-08 08:33:51 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<11;i++){
+            System.out.println("I am currently printing "+i);
+        }
+        print.printText();
+    }
+}
---
+++ src/print.java
@@ -0,0 +1,5 @@
+public class print {
+    public static void printText(){
+        System.out.println("Printed all numbers from the loop");
+    }
+}
